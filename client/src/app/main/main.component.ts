6f09e6005f22107ebb715f637534494fdf4d885a
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-11 01:11:09 +0300

    add main.component

---
+++ client/src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
+})
+
+export class MainComponent {
+    header = "Main";
+}
