4d854a709e3caf775ca5ee09758517e4d2d7b922
Quontico <32801054+Quontico@users.noreply.github.com>
2017-11-14 11:56:24 +0300

    Add POJO classes

---
+++ server/src/main/java/com/itechart/entity/Item.java
@@ -0,0 +1,59 @@
+public class Item {
+
+    private Integer id;
+    private String name;
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (!id.equals(item.id)) return false;
+        if (name != null ? !name.equals(item.name) : item.name != null) return false;
+        return type != null ? type.equals(item.type) : item.type == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+}
