7b5d7b45544c67e348549c8961cacd7dc35ff580
Vlad_Sytyi <valdis3gs@gmail.com>
2017-11-14 11:54:29 +0300

    entuties

---
+++ server/src/main/java/com/itechart/entity/CarType.java
@@ -0,0 +1,9 @@
+package com.itechart.entity;
+
+/**
+ * Created by Admin on 14.11.2017.
+ */
+public enum CarType {
+    
+    CARCASE,FRIDGE,TANK
+}
---
+++ server/src/main/java/com/itechart/entity/UserRole.java
@@ -0,0 +1,8 @@
+package com.itechart.entity;
+
+/**
+ * Created by Admin on 14.11.2017.
+ */
+public enum UserRole {
+    SYSADMIN, ADMIN, MANAGER, DISPATCHER, DRIVER, OWNER
+}
