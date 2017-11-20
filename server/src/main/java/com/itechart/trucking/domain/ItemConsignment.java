e653c486558eaf182a5f449df5bb0689b5aec95e
Alex <pavlovskiy.alexey.work@gmail.com>
2017-11-20 20:41:40 +0300

    Add Status Enum and Converter for ItemConsignment

--- server/src/main/java/com/itechart/trucking/domain/ItemConsignment.java
+++ server/src/main/java/com/itechart/trucking/domain/ItemConsignment.java
@@ -10,7 +10,15 @@ public class ItemConsignment extends AbstractPersistentObject {
     private Integer amount;
 
     @Column(name = "item_status")
-    private String status;
+    @Convert(converter = TypeConverter.class)
+    private Status status;
+
+    public enum Status {
+        REGISTERED,
+        CHECKED,
+        DELIVERED,
+        LOST
+    }
 
     @ManyToOne
     @JoinColumn(name = "items_id")
@@ -28,11 +36,11 @@ public class ItemConsignment extends AbstractPersistentObject {
         this.amount = amount;
     }
 
-    public String getStatus() {
+    public Status getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(Status status) {
         this.status = status;
     }
 
@@ -50,5 +58,41 @@ public class ItemConsignment extends AbstractPersistentObject {
 
     public void setItem(Item item) {
         this.item = item;
+    }
+
+    @Convert
+    public static class TypeConverter implements AttributeConverter<Status, String> {
+
+        @Override
+        public String convertToDatabaseColumn(Status attribute) {
+            switch (attribute) {
+                case REGISTERED:
+                    return "C";
+                case CHECKED:
+                    return "V";
+                case DELIVERED:
+                    return "D";
+                case LOST:
+                    return "L";
+                default:
+                    throw new IllegalArgumentException("Unknown " + attribute);
+            }
+        }
+
+        @Override
+        public Status convertToEntityAttribute(String dbData) {
+            switch (dbData) {
+                case "R":
+                    return Status.REGISTERED;
+                case "C":
+                    return Status.CHECKED;
+                case "D":
+                    return Status.DELIVERED;
+                case "L":
+                    return Status.LOST;
+                default:
+                    throw new IllegalArgumentException("Unknown " + dbData);
+            }
+        }
     }
 }
