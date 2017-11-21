b9e86ea3028aa2f0d25566e4baf3a5e8cd4e34eb
Vlad_Sytyi <valdis3gs@gmail.com>
2017-11-21 11:55:21 +0300

    Trying to add tests.Fixed Dao

--- server/src/test/java/com/itechart/trucking/dao/impl/JpaCarDaoTest.java
+++ server/src/test/java/com/itechart/trucking/dao/impl/JpaCarDaoTest.java
@@ -6,13 +6,21 @@ import com.itechart.trucking.dao.ClientDao;
 import com.itechart.trucking.dao.UserDao;
 import com.itechart.trucking.dao.WaybillDao;
 import com.itechart.trucking.domain.Car;
+
 import com.itechart.trucking.domain.User;
 import com.itechart.trucking.domain.Waybill;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Admin on 20.11.2017.
@@ -24,16 +32,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class JpaCarDaoTest {
 
     @Autowired
-    private ClientDao dao;
+    private CarDao carDao;
 
-    @Autowired
-    private CarDao car;
+    private Car car;
 
-    @Autowired
-    private UserDao user;
+    @Before
+    public void setUp(){
+        car = new Car();
+        car.setName("MAZ");
+        car.setConsumption(50);
+        car.setNumber("adu123AE");
+        car.setType(Car.Type.TANK);
+    }
+
+    @Test
+    public void getCarByIdShouldReturnNoCarTest() throws Exception {
 
 
+        final int invalidCarId = 100500;
+        Optional<Car> carTest = carDao.findOne(invalidCarId);
 
-    @Autowired
-    private WaybillDao waybill;
+        final String errorMessage = "Expected empty optional car object";
+        Assert.assertFalse(errorMessage, carTest.isPresent());
+
+    }
+
+    @Test
+    public void getCarsShouldReturnNoCarTest() throws Exception {
+
+      List<Car> carList =  carDao.findAll();
+
+    }
 }
--- server/src/test/java/com/itechart/trucking/dao/impl/JpaUserDaoTest.java
+++ server/src/test/java/com/itechart/trucking/dao/impl/JpaUserDaoTest.java
@@ -1,7 +1,20 @@
 package com.itechart.trucking.dao.impl;
 
+import com.itechart.trucking.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by Admin on 20.11.2017.
  */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Transactional
 public class JpaUserDaoTest {
+
+
+
 }
