be9a5bc04ff04bde07e9df783db2701da2243cde
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-23 20:44:08 +0300

    add authenthication feature on the server app

---
+++ server/src/main/java/com/itechart/trucking/dao/impl/JpaUserDao.java
@@ -0,0 +1,76 @@
+package com.itechart.trucking.dao.impl;
+
+import com.itechart.trucking.dao.UserDao;
+import com.itechart.trucking.domain.Client;
+import com.itechart.trucking.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JpaUserDao implements UserDao{
+
+    @Autowired
+    private EntityManager em;
+
+    @Override
+    public Optional<User> getUserById(Integer id) {
+        return Optional.ofNullable(em.find(User.class, id));
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        final String jpqlQuery =
+                "select u from User u where u.email = :email";
+        TypedQuery<User> query = em.createQuery(jpqlQuery, User.class);
+        query.setParameter("email", email);
+
+        User user = query.getSingleResult();
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public List<User> getUsersByPage(int offset, int recordPerPage) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> root = cq.from(User.class);
+        cq.select(root);
+        TypedQuery<User> q = em.createQuery(cq);
+        q.setFirstResult((offset - 1) * recordPerPage);
+        q.setMaxResults(recordPerPage);
+        return q.getResultList();
+    }
+
+    @Override
+    public int getUserCount() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        Root<User> root = cq.from(User.class);
+        cq.select(cb.count(root));
+
+        return em.createQuery(cq).getSingleResult().intValue();
+    }
+
+    @Override
+    public User addUser(User user) {
+        em.persist(user);
+        return user;
+    }
+
+    @Override
+    public void updateUser(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        em.remove(user);
+    }
+}
---
+++ server/src/test/java/com/itechart/trucking/dao/impl/JpaUserDaoTest.java
@@ -0,0 +1,125 @@
+package com.itechart.trucking.dao.impl;
+
+import com.itechart.trucking.Application;
+import com.itechart.trucking.dao.UserDao;
+import com.itechart.trucking.domain.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test class for the JpaClientDao.
+ *
+ * @see JpaUserDao
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Transactional
+public class JpaUserDaoTest {
+
+    @Autowired
+    private UserDao dao;
+
+    private User user;
+
+    private static final String USER_NAME = "test_email@gmail.com";
+
+    @Before
+    public void setUp(){
+        user = new User();
+        user.setEmail(USER_NAME);
+    }
+
+    @Test
+    public void getUserByIdShouldReturnNoUserTest() throws Exception {
+        final int invalidUserId = 5;
+        Optional<User> user = dao.getUserById(invalidUserId);
+
+        final String errorMessage = "Expected empty optional user object";
+        Assert.assertFalse(errorMessage, user.isPresent());
+    }
+
+    @Test
+    public void getUserByIdShouldReturnCorrespondUser() throws Exception {
+        User expectedUser = dao.addUser(user);
+        final int addedUserId = expectedUser.getId();
+        Optional<User> actualUser = dao.getUserById(addedUserId);
+
+        final String errorMessage = "Expected and actual user are different";
+        assertEquals(errorMessage, expectedUser, actualUser.orElse(new User()));
+    }
+
+    @Test
+    public void getUserByEmailShouldReturnExactUserCount() throws Exception {
+        dao.addUser(user);
+
+        User user1 = new User();
+        user1.setEmail("test@name");
+        dao.addUser(user1);
+
+        final String searchEmail = "test@email.com";
+        User expectedUser = new User();
+        expectedUser.setEmail(searchEmail);
+        dao.addUser(expectedUser);
+
+        Optional<User> actualUser = dao.getUserByEmail(searchEmail);
+
+        final String errorMessage = "Expected and actual user size are different";
+        assertEquals(errorMessage, expectedUser, actualUser.orElse(new User()));
+    }
+
+    @Test
+    public void getUsersByExistedPageShouldReturnExactUserCount() throws Exception {
+        dao.addUser(user);
+
+        User user1 = new User();
+        user1.setEmail("test name");
+        dao.addUser(user1);
+
+        final int page = 1;
+        final int pageSize = 20;
+        List<User> userList = dao.getUsersByPage(page, pageSize);
+
+        final int expectedClientCount = 2;
+        final String errorMessage = "Expected and actual user size are different";
+        assertEquals(errorMessage, expectedClientCount, userList.size());
+    }
+
+    @Test
+    public void afterUpdateClientGetClientByIdShouldReturnCorrespondClient() throws Exception {
+        dao.addUser(user);
+        user.setEmail("new test name");
+        dao.updateUser(user);
+
+        Optional<User> actualUser = dao.getUserById(user.getId());
+
+        final String errorMessage = "Expected and actual user are different";
+        assertEquals(errorMessage, user, actualUser.orElse(new User()));
+    }
+
+    @Test
+    public void afterDeleteClientGetClientCountShouldReturnCorrespondClientCount() throws Exception {
+        dao.addUser(user);
+        dao.deleteUser(user);
+        final int expectedClientCount = 0;
+        final int actualClientCount = dao.getUserCount();
+
+        final String errorMessage = "Expected and actual user size are different";
+        assertEquals(errorMessage, expectedClientCount, actualClientCount);
+    }
+
+    @Test
+    public void deleteClientWithNoIdShouldNotThrowException() throws Exception {
+        dao.deleteUser(user);
+    }
+}
