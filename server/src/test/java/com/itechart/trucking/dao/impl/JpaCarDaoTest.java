b157a0fdf4fb735ae4eec0f161a287fd4686e4d1
Vlad_Sytyi <valdis3gs@gmail.com>
2017-12-08 23:03:38 +0300

    Changed JpaCarDaoTest

--- server/src/test/java/com/itechart/trucking/dao/impl/JpaCarDaoTest.java
+++ server/src/test/java/com/itechart/trucking/dao/impl/JpaCarDaoTest.java
@@ -37,19 +37,6 @@ public class JpaCarDaoTest {
     @Autowired
     private CarDao carDao;
 
-    private Car car;
-
-    private static String NAME="Renault";
-    private static String NUMBER="2244 KE-7";
-    private static Integer ID = 5;
-
-    @Before
-    public void setUp() {
-        car = new Car();
-        car.setName(NAME);
-        car.setNumber(NUMBER);
-        car.setId(ID);
-    }
 
     @Test
     public void findCarByIdWhichNotExistsShouldThrowNullPointerException() throws Exception{
@@ -58,13 +45,7 @@ public class JpaCarDaoTest {
         assertFalse("No object expected", testCar.isPresent());
     }
 
-    @Test
-    public void findOneShouldReturnCorrespondObject() throws Exception{
-        int testId = 5;
-        Optional<Car> testCar = carDao.findOne(testId);
-        assertTrue("Expected an object", testCar.isPresent());
-        assertEquals("Expected equals objects", car, testCar.orElse(new Car()));
-    }
+
 
 
 
