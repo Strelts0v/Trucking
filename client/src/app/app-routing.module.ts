4ab26d0b756938ff3c08de92ea50546831d54775
AndreyKamutovski <kv_island@mail.ru>
2017-12-12 12:12:29 +0300

    Fix Main page.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { MainComponent } from './main';
-import { AuthGuard } from './users';
-import { RoleGuard } from './users';
-import { AuthComponent, UserListComponent } from './users/index';
-import { InvoiceListComponent } from './invoices/invoice-list/invoice-list.component';
-import { WaybillListComponent } from './waybills/waybill-list/waybill-list.component';
-import { ClientListComponent } from './clients/index';
+import {MainComponent} from './main';
+import {AuthGuard} from './users';
+import {RoleGuard} from './users';
+import {AuthComponent, UserListComponent} from './users/index';
+import {InvoiceListComponent} from './invoices/invoice-list/invoice-list.component';
+import {WaybillListComponent} from './waybills/waybill-list/waybill-list.component';
+import {ClientListComponent} from './clients/index';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 const routes: Routes = [
   {path: 'auth', component: AuthComponent},
   {
     path: '', component: MainComponent, data: {title: 'Trucking'}, canActivate: [AuthGuard],
-     children: [
-      {path: 'users', component: UserListComponent, data: {title: 'Users'}, canActivate: [RoleGuard] },
+    children: [
+      {path: '', component: WelcomeComponent, data: {title: 'Welcome'}, canActivate: [RoleGuard]},
+      {path: 'users', component: UserListComponent, data: {title: 'Users'}, canActivate: [RoleGuard]},
       {path: 'invoices', component: InvoiceListComponent, data: {title: 'Consignment notes'}, canActivate: [RoleGuard]},
       {path: 'waybills', component: WaybillListComponent, data: {title: 'Waybills'}, canActivate: [RoleGuard]},
       {path: 'clients', component: ClientListComponent, data: {title: 'Clients'}, canActivate: [RoleGuard]}
