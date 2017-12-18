43019ab5492cf68547fec6b44e9c9b52c71a8cc4
vtrusevich <gleb.streltsov.4by@gmail.com>
2017-12-18 13:57:33 +0300

    update security feature and client & user controllers logic

--- server/src/main/java/com/itechart/trucking/config/SecurityConfig.java
+++ server/src/main/java/com/itechart/trucking/config/SecurityConfig.java
@@ -58,15 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
 
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/*").hasAnyRole(
-                    AUTHORIZED_ROLE_SYSADMIN,
-                    AUTHORIZED_ROLE_ADMIN,
-                    AUTHORIZED_ROLE_MANAGER,
-                    AUTHORIZED_ROLE_DISPATCHER,
-                    AUTHORIZED_ROLE_DRIVER,
-                    AUTHORIZED_ROLE_OWNER
-                )
-                // TODO test and enable .anyRequest().fullyAuthenticated()
+                .antMatchers("/api/*").fullyAuthenticated()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new Http401AuthenticationEntryPoint("'Bearer token_type=\"JWT\"'"));
